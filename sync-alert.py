e2677a3f839ec34b4de3fa0dfa1df21a5ea1d89a
evade.ninja <travis.marks@byu.edu>
2017-04-14 13:50:51 -0600

    updated error modes

--- sync-alert.py
+++ sync-alert.py
@@ -7,46 +7,40 @@ logpath = os.environ['LOGPATH']
 
 logname = "2017-04-12.log"
 #lookback = "2017-04-12 17:46:00"
-lookback = datetime(2017,04,12,17,46,00)
+lookback = datetime(2017,04,12,17,47,00)
 
 pdate = re.compile('(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})')
-perror = re.compile('(ERROR)')
+perror = re.compile('ERROR')
 papi = re.compile('Number of UMAPI actions sent \(total, success, error\): \(\d+, \d+, (\d+)\)')
-
+pcrit = re.compile('CRITICAL')
 
 msgbody = ""
 
 with open(logpath + logname, 'r') as f:
  for line in f:
  #is this line from less than 5 minutes in the past?
- #m = re.search("(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})", line)
   m = pdate.match(line)
   if m is not None:
-   #print m.group()
-   #s = m.group() + ""
    lt = datetime.strptime(m.group(), "%Y-%m-%d %H:%M:%S")
    tdelta = lookback - lt
    if tdelta.seconds < 300:
-    #print "time criteria met"
-    me = perror.match(line)
-    if me is not None:
+    if perror.search(line): #me is not None:
      #this line contained "ERROR"
-     msgbody = msgbody + line + "\n"
-     print "er"
+     msgbody = msgbody + line
 
-    ma = papi.match(line)
-    if ma is not None:
-     if ma.group() > 0:
+    ma = papi.search(line)
+    if ma is not None: # is not None:
+     ma.group
+     if int(ma.group(1)) > 0:
+      print "ma: " + ma.group(1)
       #this line contained the results, and there were errors
-      msgbody = msgbody + line + "\n"
-      print "er1"
+      msgbody = msgbody + line
 
-   #else:
-    #print "time criteria not met"
+    if pcrit.search(line):
+      msgbody = msgbody + line
 
 if msgbody != '':
- #someting wong
+ #something wrong
  print msgbody
 else:
  print "Success!"
-
