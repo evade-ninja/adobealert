c56d82c88fd0afff6499fe8c367f2a8689d6b1fc
evade.ninja <travis.marks@byu.edu>
2017-04-14 13:29:55 -0600

    added logic (but not complete) for testing errors

--- sync-alert.py
+++ sync-alert.py
@@ -9,21 +9,44 @@ logname = "2017-04-12.log"
 #lookback = "2017-04-12 17:46:00"
 lookback = datetime(2017,04,12,17,46,00)
 
-p = re.compile('(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})')
+pdate = re.compile('(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})')
+perror = re.compile('(ERROR)')
+papi = re.compile('Number of UMAPI actions sent \(total, success, error\): \(\d+, \d+, (\d+)\)')
+
+
+msgbody = ""
 
 with open(logpath + logname, 'r') as f:
-	for line in f:
-		#is this line from less than 5 minutes in the past?
-		#m = re.search("(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})", line)
-		m = p.match(line)
-		#print m.group()
-		#s = m.group() + ""
-		lt = datetime.strptime(m, "%Y-%m-%d %H:%M:%S")
-		tdelta = lookback - lt
-		if tdelta.seconds < 300:
-			print "time criteria met"
+ for line in f:
+ #is this line from less than 5 minutes in the past?
+ #m = re.search("(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})", line)
+  m = pdate.match(line)
+  if m is not None:
+   #print m.group()
+   #s = m.group() + ""
+   lt = datetime.strptime(m.group(), "%Y-%m-%d %H:%M:%S")
+   tdelta = lookback - lt
+   if tdelta.seconds < 300:
+    #print "time criteria met"
+    me = perror.match(line)
+    if me is not None:
+     #this line contained "ERROR"
+     msgbody = msgbody + line + "\n"
+     print "er"
 
+    ma = papi.match(line)
+    if ma is not None:
+     if ma.group() > 0:
+      #this line contained the results, and there were errors
+      msgbody = msgbody + line + "\n"
+      print "er1"
 
-#print line
+   #else:
+    #print "time criteria not met"
 
+if msgbody != '':
+ #someting wong
+ print msgbody
+else:
+ print "Success!"
 
